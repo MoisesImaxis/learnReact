45b150524bea29ab8569d99e25fc42b6dc9e3573
Moises <moises@imaxis.com.br>
2024-01-30 14:08:02 -0300

    começo da colorização da pagian

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "./styles/generic/reset.css";
+import "./styles/elements/base.css";
+import "./styles/settings/color.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
